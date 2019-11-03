8805613616852ff35e670c2b953f72339c8f7213
Kirpalsinh9 <kirpalsinhji976@yahoo.com>
2019-11-03 16:53:27 -0500

    Changed Sol files

--- submissions/SmartContract/Main.sol
+++ submissions/SmartContract/Main.sol
@@ -3,35 +3,47 @@ pragma solidity ^0.5.0;
 
 import "./Rewards.sol";
 
-contract Main
+contract Sponsors
 {
+     struct sponsor {
+        bool issponsor;
+        uint256 sponsorship;
+        
+    }
+   
+    address public sponsorshipContractAddress=address(this);
+    
+    mapping(address => sponsor) public sponsorsDB;
+    Rewards public rewardsContract;
+    
+    constructor(address _rewardsContract) public {
+        rewardsContract=Rewards(_rewardsContract);
+        if(!rewardsContract.isMinter(sponsorshipContractAddress)) {
+            rewardsContract.addMinters(sponsorshipContractAddress);
+        }
+       
+       
+       sponsorshipContractAddress = address(this);
+       
+    }
+    
     modifier isAuthorized() {
-   require(isMinter(msg.sender));
-  _;
-}
-
-// @dev function to add Sponsor.
-
-function addSponsors(address _sponsor) external returns (bool) {
-
-}
-
-// @dev function to remove Sponsor.
-
-function removeSponsors(address _sponsor) external returns (bool) {
-
-}
-
-
-
-function trigger() external isAuthorized returns (bool) {
-
-}
-
-// @dev function to withdraw tokens.
-
-function withdraw() external isAuthorized returns (bool) {
-
-}
-
+        require(sponsorsDB[msg.sender].issponsor, "caller is not a authorized Sponsor");
+        _; 
+    }
+    function addSponsor(address _sponsor) public {
+        require(!sponsorsDB[msg.sender].issponsor, "It's already added");
+        sponsorsDB[_sponsor].issponsor = true;
+        
+    }
+    
+    function removeSponsor(address _sponsor) public {
+        require(sponsorsDB[_sponsor].issponsor, "It's already removed");
+        sponsorsDB[_sponsor].issponsor = false;
+    }
+    
+    function sponsoring() public isAuthorized payable {
+        sponsorsDB[msg.sender].sponsorship += msg.value;
+    
+    }
 }
